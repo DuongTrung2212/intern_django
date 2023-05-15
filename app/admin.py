e66306b4d87c4d1bbe5a7d2beb91c628d0e12c4d
DuongTrung2212 <duongtrungqb12@gmail.com>
2023-05-15 16:50:20 +0700

    first commit

---
+++ app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(User)
+admin.site.register(Ticket)
+admin.site.register(Tour)
+admin.site.register(Staff)
+admin.site.register(Category)
